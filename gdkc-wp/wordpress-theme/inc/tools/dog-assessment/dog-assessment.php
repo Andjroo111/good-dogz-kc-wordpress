7a5c67590ceed778bf32af800faa216cf1d0309c
Andjroo111 <andrewslattner@gmail.com>
2025-04-03 14:31:45 -0500

    Add dog assessment tool main file

---
+++ gdkc-wp/wordpress-theme/inc/tools/dog-assessment/dog-assessment.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Dog Assessment Tool
+ *
+ * This file implements the dog assessment tool for Good Dogz KC.
+ * It allows clients to assess their dog's behavior and training needs.
+ */
+
+// Include necessary dependencies
+require_once( 'process-assessment.php' );
+
+// This is a placeholder. The actual implementation will be added later.
+function gdkc_dog_assessment_init() {
+    // Initialize the assessment tool
+}
+
+add_action( 'init', 'gdkc_dog_assessment_init' );
