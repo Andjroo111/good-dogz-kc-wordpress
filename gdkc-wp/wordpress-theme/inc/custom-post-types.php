507b162336a5e63a799f67d752e782460ca9939b
Andjroo111 <andrewslattner@gmail.com>
2025-04-03 14:31:35 -0500

    Add custom post types file

---
+++ gdkc-wp/wordpress-theme/inc/custom-post-types.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Custom Post Types for Good Dogz KC
+ *
+ * This file defines custom post types for training programs, success stories, and resources.
+ */
+
+// Placeholder content that will be replaced with the actual file content
